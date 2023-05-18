1f18a8c94a96b4c8d5fee1d257d23d286bc43eef
njiddasalifu <njiddasalifu@gmail.com>
2023-05-18 11:28:21 +0100

    public getters and setters

--- java/Student.java
+++ java/Student.java
@@ -4,4 +4,20 @@ public class Student {
 
     int age;
     String address;
+
+    public Student(String name, int age, String address){
+        this.name = name;
+        this.age =age;
+        this.address = address;
+    }
+    //declaring the setters and getters for modifying the attributes
+    public void  setName(String name){
+        this.name = name;
+    }
+    public  void  setAge(int age){
+        this.age = age;
+    }
+    public  void setAddress(String address){
+        this.address = address;
+    }
 }
